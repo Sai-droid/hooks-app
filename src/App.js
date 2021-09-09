9f9ae326482184488949d036b083b45df70f60a4
Saicharan <kalakonda.saicharan@capgemini.com>
2021-09-09 15:21:12 +0530

    styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-       hello world
+      <header className="App-header">
+        <h1 className="title name">Names List</h1>
+        <h3 className ="name">Peter</h3>
+        <h3 className ="name">John</h3>
+        <h3 className ="name">Jill</h3>
+
+        </header> 
     </div>
   );
 }
