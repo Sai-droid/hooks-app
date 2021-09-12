e27e76986ef298623a2c63a9097ca5b0ea92ca69
Saicharan <kalakonda.saicharan@capgemini.com>
2021-09-12 18:31:40 +0530

    event handling

--- src/Components/CalorieCard.js
+++ src/Components/CalorieCard.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import  './Calories.css'
-
+import React from "react";
+import "./Calories.css";
+import Calories from "./Calories";
 
 function CalorieCard(props) {
-    return (
-        <div className="item-style">
-            <h3>{props.item.name}</h3>
-            <h3>{props.item.calorie}</h3>
-            <button name={props.item.name} className="remove-button" onClick={props.onClick}>RemoveItem</button>
-        </div>
-    )
+  return (
+    <div className="item-style">
+      {props.editt ? (
+        <input type="text" onKeyPress ={(e)=>props.onKeyPress(e,props.index)}  defaultValue={props.item.name} />
+      ) : (
+        <h3  onDoubleClick={props.onDoubleClick}>{props.item.name}</h3>
+      )}
+
+      <h3>{props.item.calorie}</h3>
+      <button
+        name={props.item.name}
+        className="remove-button"
+        onClick={props.onClick}
+      >
+        RemoveItem
+      </button>
+    </div>
+  );
 }
 
-export default CalorieCard
+export default CalorieCard;
--- src/Components/Calories.js
+++ src/Components/Calories.js
@@ -1,47 +1,67 @@
-import React ,{useState}from 'react'
-import CalorieCard from './CalorieCard'
+import React, { useState } from "react";
+import CalorieCard from "./CalorieCard";
 
-
-
-
-const initList =[
-    {
-        name:"tomato",calorie:20
-    },
-    {
-        name:"rice",calorie:30
-    },
-    {
-        name:"Candy",calorie:100
-    }
-]
+const initList = [
+  {
+    name: "tomato",
+    calorie: 20,
+  },
+  {
+    name: "rice",
+    calorie: 30,
+  },
+  {
+    name: "Candy",
+    calorie: 100,
+  },
+];
 
 export default function Calories() {
-    const [list,setList] = useState(initList)
-    const removeUnhealthy =(e)=>{
-        const filteredList = list.filter((item)=>item.calorie<=50)
-        setList(filteredList)
-    }
-    const defaultList =(e)=>{      
-        setList(initList)
-    }
-    const removeItem =(e)=>{  
-        console.dir(e.target.name);
-        const filteredList = list.filter((item)=>e.target.name!==item.name)
-        setList(filteredList)
-    }
+  const [list, setList] = useState(initList);
+  const [edit, setEdit] = useState(false);
 
-    return (
-        <div >
-            <h2>Grocery list </h2>
-           {
-               list.map((v,i)=>{
-                   return(<CalorieCard key ={`${i}${v.name}${v.calorie}`} item ={v} onClick ={removeItem} ></CalorieCard>)
-               })
-           }
-           <button onClick ={removeUnhealthy}>RemoveUnhealthy</button>
-           <button onClick ={defaultList}>defaultList</button>
-        </div>
-    )
+  const removeUnhealthy = (e) => {
+    const filteredList = list.filter((item) => item.calorie <= 50);
+    setList(filteredList);
+  };
+  const defaultList = (e) => {
+    setList(initList);
+  };
+  const editableText = (e) => {
+    setEdit(true);
+  };
+
+  const defaultText = (e, i) => {
+    if (e.key === "Enter") {
+      setEdit(!edit);
+      const copyList = [...list];
+      copyList[i].name = e.target.value;
+    }
+  };
+  const removeItem = (e) => {
+    console.dir(e.target.name);
+    const filteredList = list.filter((item) => e.target.name !== item.name);
+    setList(filteredList);
+  };
+
+  return (
+    <div>
+      <h2>Grocery list </h2>
+      {list.map((v, i) => {
+        return (
+          <CalorieCard
+            key={`${i}${v.name}${v.calorie}`}
+            item={v}
+            onClick={removeItem}
+            editt={edit}
+            onDoubleClick={editableText}
+            onKeyPress={defaultText}
+            index={i}
+          ></CalorieCard>
+        );
+      })}
+      <button onClick={removeUnhealthy}>RemoveUnhealthy</button>
+      <button onClick={defaultList}>defaultList</button>
+    </div>
+  );
 }
-
